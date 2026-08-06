60c6886901306a1f0eaff78ec51b7d7900ef36a6
michael <michael@dragoninventor.com>
2026-08-06 15:47:19 -0600

    Add Google Tag Manager scripts to layout for analytics tracking

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/styles/fonts";
 import { Providers } from "@/components/Providers";
 import { Cart } from "@/components/cart";
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: "Angela's Store",
@@ -27,11 +28,25 @@ export default function RootLayout({
 			<body
 				className={`${font_default.variable} ${font_hero.variable} ${font_logo.variable} ${font_sitenav.variable} ${font_default.className} bg-vanilla-50`}
 			>
-			<Providers>
-				<SiteHeader />
-				{children}
-				<Cart />
-			</Providers>
+				<Providers>
+					<SiteHeader />
+					{children}
+					<Cart />
+				</Providers>
+				{/* Google tag */}
+				<Script
+					src={
+						"https://www.googletagmanager.com/gtag/js?id=AW-18311865890"
+					}
+					strategy={"afterInteractive"}
+				/>
+				<Script id={"gtag"} strategy={"afterInteractive"}>
+					{`window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'AW-18311865890');
+				`}
+				</Script>
 			</body>
 		</html>
 	);
